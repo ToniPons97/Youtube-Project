f7571cd97dd49b20ff1050f1156e758acf97f6df
Antonio Pons <toninopons@gmail.com>
2019-11-09 20:50:20 +0100

    still trying to understang the API

--- src/api.py
+++ src/api.py
@@ -37,3 +37,13 @@ def get_authenticated_service():
 
 
 service = get_authenticated_service()
+
+
+def search_videos_by_keyword(service, **kwargs):
+    results = service.search().list(**kwargs).execute()
+    for item in results['items']:
+        print('%s - %s' % (item['snippet']['title'], item['id']['videoId']))
+
+
+keyword = input('Enter a keyword: ')
+search_videos_by_keyword(service, q=keyword, part='id,snippet', eventType='completed', type='video')
