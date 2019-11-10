d6132bd4236791598392bb5fb81e0e89bcd1d277
Antonio Pons <toninopons@gmail.com>
2019-11-10 16:58:49 +0100

    Almost done with

--- src/api.py
+++ src/api.py
@@ -39,18 +39,16 @@ def get_authenticated_service():
 service = get_authenticated_service()
 
 title = []
+videoId = []
 channelId = []
 channelTitle = []
 categoryId = []
-videoId = []
 viewCount = []
+favoriteCount = []
 likeCount = []
 dislikeCount = []
 commentCount = []
-favoriteCount = []
-category = []
-tags = []
-videos = []
+
 
 
 def search_videos_by_keyword(service, **kwargs):
@@ -65,7 +63,34 @@ def search_videos_by_keyword(service, **kwargs):
         categoryId.append(stats['items'][0]['snippet']['categoryId']) 
         favoriteCount.append(stats['items'][0]['statistics']['favoriteCount'])
         viewCount.append(stats['items'][0]['statistics']['viewCount'])
-    print(title, viewCount)
+
+        try:
+            likeCount.append(stats['items'][0]['statistics']['likeCount'])
+        except:
+            print("Video titled {0}, on Channel {1} Likes Count is not available".format(stats['items'][0]['snippet']['title'], stats['items'][0]['snippet']['channelTitle']))
+            print(stats['items'][0]['statistics'].keys())
+            likeCount.append("Not available")
+                
+        try:
+            dislikeCount.append(stats['items'][0]['statistics']['dislikeCount'])     
+        except:
+            print("Video titled {0}, on Channel {1} Dislikes Count is not available".format(stats['items'][0]['snippet']['title'], stats['items'][0]['snippet']['channelTitle']))
+            print(stats['items'][0]['statistics'].keys())
+            dislikeCount.append("Not available")
+
+        if 'commentCount' in stats['items'][0]['statistics'].keys():
+            commentCount.append(stats['items'][0]['statistics']['commentCount'])
+        else:
+            commentCount.append(0)
+
+    youtube_dict = {'channelId': channelId,'channelTitle': channelTitle,
+                    'categoryId':categoryId,'title':title,'videoId':videoId,
+                    'viewCount':viewCount,'likeCount':likeCount,'dislikeCount':dislikeCount,
+                    'commentCount':commentCount,'favoriteCount':favoriteCount}
+ 
+    return youtube_dict
+
+
 keyword = input('Enter a keyword: ')
 search_videos_by_keyword(service, q=keyword, part='id,snippet', eventType='completed', type='video', order='viewCount')
 
