054945792db2669b45e4b764f1a3565edbcfdf0d
Antonio Pons <toninopons@gmail.com>
2019-11-11 12:29:01 +0100

    Implementing more arguments for argparse

--- src/clean.py
+++ src/clean.py
@@ -1,14 +1,22 @@
 import pandas as pd
 import main
 
-df = pd.read_csv("../input/USvideos.csv")
-print(df.head())
 
 def dict_to_df(data):
-    df = pd.DataFrame(data)
-    print("Shape of DataFrame", df.shape)
-    return df
+    return pd.DataFrame(data)
+
      
 
-def df_to_csv(file_name):
-    return df.to_csv(file_name, sep='\t', encoding='utf-8', index=False)
+#def df_to_csv(file_name):
+#    return pd.to_csv(file_name, sep='\t', encoding='utf-8', index=False)
+
+
+def show_all_columns(data):
+    return data.columns
+
+
+def show_specified_columns(data, *args):
+    return data[[args]]
+
+
+
--- src/main.py
+++ src/main.py
@@ -8,7 +8,8 @@ import clean
 def recieveConfig():
     parser = argparse.ArgumentParser(description='Youtube data extractor')
     parser.add_argument('-s', '--search', help='Make a Youtube search by doing --search <keyword>.')
-    parser.add_argument('-n', '--number', help='Specify maximum number of results to obtain in request', default=25)        
+    parser.add_argument('-n', '--number', help='Specify maximum number of results to obtain in request', default=25)
+    parser.add_argument('-shc', '--showColumns', help='Prints list of all existing columns in DataFrame (Admitted values: True, False.)', default=False)     
     args = parser.parse_args()
     return args
 
@@ -16,12 +17,15 @@ def main():
     
     print("Youtube Data Extractor!!!")
     config = recieveConfig()
-    
-  
     query = api.search_videos_by_keyword(api.service, q=config.search, part='id,snippet', eventType='completed', type='video', order='viewCount', maxResults=config.number)
-
     generated_df = clean.dict_to_df(query)
     print(generated_df)
+    
+    if config.showColumns:
+        clean.show_all_columns(generated_df)
+    else:
+        pass
+
 
 
 if __name__=="__main__":
