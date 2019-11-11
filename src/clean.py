4ce4d117fa212cc417e7603a557f305b50083bfb
Antonio Pons <toninopons@gmail.com>
2019-11-11 11:35:50 +0100

    Converting results to DataFrames automatically

--- src/clean.py
+++ src/clean.py
@@ -4,4 +4,11 @@ import main
 df = pd.read_csv("../input/USvideos.csv")
 print(df.head())
 
+def dict_to_df(data):
+    df = pd.DataFrame(data)
+    print("Shape of DataFrame", df.shape)
+    return df
+     
 
+def df_to_csv(file_name):
+    return df.to_csv(file_name, sep='\t', encoding='utf-8', index=False)
--- src/main.py
+++ src/main.py
@@ -3,10 +3,12 @@ import api
 import sys
 import argparse
 import subprocess
+import clean
 
 def recieveConfig():
     parser = argparse.ArgumentParser(description='Youtube data extractor')
-    parser.add_argument('-s', '--search', help='Make a Youtube search by doing --search <keyword>.')             
+    parser.add_argument('-s', '--search', help='Make a Youtube search by doing --search <keyword>.')
+    parser.add_argument('-n', '--number', help='Specify maximum number of results to obtain in request', default=25)        
     args = parser.parse_args()
     return args
 
@@ -15,11 +17,11 @@ def main():
     print("Youtube Data Extractor!!!")
     config = recieveConfig()
     
-    
-    query = api.search_videos_by_keyword(api.service, q=config.search, part='id,snippet', eventType='completed', type='video', order='viewCount', relevanceLanguage = 'en')
+  
+    query = api.search_videos_by_keyword(api.service, q=config.search, part='id,snippet', eventType='completed', type='video', order='viewCount', maxResults=config.number)
 
-    print(query)
-
+    generated_df = clean.dict_to_df(query)
+    print(generated_df)
 
 
 if __name__=="__main__":
